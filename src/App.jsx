63c42f802438caff8730b9601341342617750184
Sarah Schneider <mureb_rosa@hotmail.com>
2024-02-20 16:34:28 -0300

    start project, build layout and username functionality

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import { Player } from "./components/Player";
+
+function App() {
+	return (
+    <>
+      <header>
+        <img src="game-logo.png" alt="Tic-Tac-Toe Logo" />
+        <h1>Tic Tac Toe</h1>
+      </header>
+      <main>
+        <div id="game-container">
+          {/* PLAYERS */}
+          <ol id="players">
+            <Player playerName="Player 01" symbol="X" />
+            <Player playerName="Player 02" symbol="O" />
+          </ol>
+          
+          {/* GAME BOARD */}
+        </div>
+
+        {/* GAME LOG */}
+
+      </main>
+    </>
+	);
+}
+
+export default App;
---
+++ src/components/Player.jsx
@@ -0,0 +1,29 @@
+import { useState } from "react";
+
+export function Player({ playerName, symbol }) {
+    const [ isEditing, setIsEditing ] = useState(false);
+    const [ name, setName ] = useState("");
+
+    function handleEditing () {
+        setIsEditing(isEditing => !isEditing);
+    };
+
+    function handleSettingPlayerName (event) {
+        setName(event.target.value);
+    };
+
+	return (
+		<li>
+			<span className="player">
+                {isEditing ? 
+                    <input type="text" onChange={handleSettingPlayerName} value={name} placeholder={name === "" ? playerName : name} /> : 
+                    <span className="player-name">{name === "" ? playerName : name}</span>
+                } 
+				<span className="player-symbol">{symbol}</span>
+			</span>
+            <button onClick={handleEditing}>
+                {isEditing ? "Save" : "Edit"}
+            </button>	
+		</li>
+	);
+}
