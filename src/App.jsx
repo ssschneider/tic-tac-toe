ca409df8309eecaab1ec109f4b1278b335fb56d6
Sarah Schneider <mureb_rosa@hotmail.com>
2024-02-21 16:32:22 -0300

    improve active player management

--- src/App.jsx
+++ src/App.jsx
@@ -3,55 +3,73 @@ import { GameBoard } from "./components/Gameboard";
 import { Player } from "./components/Player";
 import { PlayLog } from "./components/Log";
 
+function deriveActivePlayer(gameLog) {
+	let activePlayer = "X";
+
+	if (gameLog.length > 0 && gameLog[0].player === "X") {
+		activePlayer = "O";
+	}
+
+	return activePlayer;
+}
+
 function App() {
-  const [ activePlayer, setActivePlayer ] = useState("X");
-  const [ gameLog, setGameLog ] = useState([]);
+	const [gameLog, setGameLog] = useState([]);
 
-  function handlePlayerSwitch (rowIndex, columnIndex) {
-    setActivePlayer(currentlyActivePlayer => currentlyActivePlayer === "X" ? "O" : "X");
-    setGameLog(prevGameLog => {
-      let currentPlayer = "X";
+	const activePlayer = deriveActivePlayer(gameLog);
 
-      if (prevGameLog.length > 0 && prevGameLog[0].player === "X") {
-          currentPlayer = "O"
-      };
+	function handlePlayerSwitch(rowIndex, columnIndex) {
+		setGameLog(prevGameLog => {
+			const currentPlayer = deriveActivePlayer(prevGameLog);
 
-      const updatedGameLog = [
-        { square: {
-            row: rowIndex,
-            column: columnIndex
-          }, player: currentPlayer
-        } 
-        ,...prevGameLog
-      ];
+			const updatedGameLog = [
+				{
+					square: {
+						row: rowIndex,
+						column: columnIndex,
+					},
+					player: currentPlayer,
+				},
+				...prevGameLog,
+			];
 
-      return updatedGameLog;
-    });
-
-  };
+			return updatedGameLog;
+		});
+	}
 
 	return (
-    <>
-      <header>
-        <img src="game-logo.png" alt="Tic-Tac-Toe Logo" />
-        <h1>Tic Tac Toe</h1>
-      </header>
-      <main>
-        <div id="game-container">
-          {/* PLAYERS */}
-          <ol id="players" className="highlight-player" >
-            <Player playerName="Player 01" symbol="X" isActive={activePlayer === "X"} />
-            <Player playerName="Player 02" symbol="O" isActive={activePlayer === "O"}/>
-          </ol>
-          
-          {/* GAME BOARD */}
-          <GameBoard onPlayerSwitch={handlePlayerSwitch} plays={gameLog} />
-        </div>
+		<>
+			<header>
+				<img src="game-logo.png" alt="Tic-Tac-Toe Logo" />
+				<h1>Tic Tac Toe</h1>
+			</header>
+			<main>
+				<div id="game-container">
+					{/* PLAYERS */}
+					<ol id="players" className="highlight-player">
+						<Player
+							playerName="Player 01"
+							symbol="X"
+							isActive={activePlayer === "X"}
+						/>
+						<Player
+							playerName="Player 02"
+							symbol="O"
+							isActive={activePlayer === "O"}
+						/>
+					</ol>
 
-        {/* GAME LOG */}
-        <PlayLog gameLog={gameLog} />
-      </main>
-    </>
+					{/* GAME BOARD */}
+					<GameBoard
+						onPlayerSwitch={handlePlayerSwitch}
+						plays={gameLog}
+					/>
+				</div>
+
+				{/* GAME LOG */}
+				<PlayLog gameLog={gameLog} />
+			</main>
+		</>
 	);
 }
 
